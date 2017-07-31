723fe51b0305ddb8afcdbe87759277a6eedb5330
jafow <jared.a.fowler@gmail.com>
2017-07-31 14:11:46 -0700

    Add header and nav

--- views/main.js
+++ views/main.js
@@ -1,16 +1,31 @@
 var html = require('choo/html')
 
-var TITLE = '🚂🚋🚋'
-
 module.exports = view
 
 function view (state, emit) {
-  if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
+  // if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
+  let newState = Object.assign({}, state)
+  newState.label = 'weight'
   return html`
     <body class="sans-serif">
-      <h1 class="f-headline pa3 pa4-ns">
-        Choo choo!
-      </h1>
+      <header>
+        <nav>
+        <ul class="nav-menu">
+          <li><a href="/update">Update</a></li>
+          <li><a href="/delete">Delete</a></li>
+        </ul> 
+        </nav>
+        <h3 class="f-headline pa3 pa4-ns">PLE Stats</h3>
+      </header>
+      <section>
+        <p><label>${newState.label}</label></p>
+      </section>
+      <footer>
+      </footer>
     </body>
   `
+}
+
+function foo () {
+  console.log('clicked')
 }
