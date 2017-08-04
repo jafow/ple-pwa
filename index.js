b910e0fa82b8886637f31d8fc164a6747eb50955
jafow <jared.a.fowler@gmail.com>
2017-08-04 13:46:50 -0700

    pre-ssr

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ var css = require('sheetify')
 var choo = require('choo')
 
 css('tachyons')
+css('./assets/styles/header-bkground.css')
 
 var app = choo()
 if (process.env.NODE_ENV !== 'production') {
   app.use(require('choo-devtools')())
   app.use(require('choo-log')())
 }
-app.use(require('choo-service-worker')())
+// app.use(require('choo-service-worker')())
 
 app.route('/', require('./views/main'))
+app.route('/posted', function (data) {
+  console.log('data!')
+  console.log(data)
+  return '<body><h1>hi</h1></body>'
+})
 app.route('/*', require('./views/404'))
 
 if (!module.parent) app.mount('body')
