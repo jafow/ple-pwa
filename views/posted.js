d5c01ef4094f031ed8c1550a023feda35ade8585
jafow <jared.a.fowler@gmail.com>
2017-08-10 21:52:14 -0700

    Pull the logic for rendering `opts` and `notes` out of the return
    Looked ugly

--- views/posted.js
+++ views/posted.js
@@ -3,32 +3,41 @@ var html = require('choo/html')
 module.exports = posted
 
 function posted (state, emit) {
-  let { date, weight, arm, abdomen, foot } = state.parsed
+  var { date, weight, arm, abdomen, foot } = state.parsed
+
+  var opts = state.parsed.options.length > 0
+    ? html`<div>
+            <h4>options: </h4>
+            <p>${state.parsed.options.join(',')}</p>
+          </div>`
+    : ''
+
+  var notes = state.parsed.notes 
+    ? html`<div>
+            <h4> Notes: </h4>
+            <p>${state.parsed.notes}</p>
+          </div>`
+    : ''
+
   return html`
-  <body>
-    <h1>This has been saved:</h1>
-    <a href="/">Home</a>
-    <a href="/update">Update</a>
-    <a href="/history">History</a>
-    <section>
-      <ul>
-        <li>Date: ${date}</li>
-        <li>Weight: ${weight}</li>
-        <li>Arm Size: ${arm}</li>
-        <li>Abdomen Size: ${abdomen}</li>
-        <li>Foot Size: ${foot}</li>
-      </ul>
-      <div>
-        ${
-          (state.parsed.options.length > 0)
-          ? html`<p> options: </p>
-            <p>${state.parsed.options.join(',')}</p>`
-          : ''
-        }
-        <p>Notes: </p>
-        <p>${state.parsed.notes}</p>
-      </div>
-    </section>
-  </body>
+    <body>
+      <h1>This has been saved:</h1>
+      <a href="/">Home</a>
+      <a href="/update">Update</a>
+      <a href="/history">History</a>
+      <section>
+        <div>
+          <ul>
+            <li>Date: ${date}</li>
+            <li>Weight: ${weight}</li>
+            <li>Arm Size: ${arm}</li>
+            <li>Abdomen Size: ${abdomen}</li>
+            <li>Foot Size: ${foot}</li>
+          </ul>
+        </div>
+        ${ opts }
+        ${ notes }
+      </section>
+    </body>
   `
 }
