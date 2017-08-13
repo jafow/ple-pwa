98c9c14cc04af3bb95d64eb7072440c9e8c7e746
jafow <jared.a.fowler@gmail.com>
2017-08-13 10:10:01 -0700

    Setup server routes

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-var css = require('sheetify')
-var choo = require('choo')
-var express = require('express')
-var server = express()
-var bodyParser = require('body-parser')
+const css = require('sheetify')
+const choo = require('choo')
+const express = require('express')
+const server = express()
+const bodyParser = require('body-parser')
+const DbController = require('./lib/setup-db')
+const dbCtrl = DbController('v1.sql')
+const stateCtrl = require('./lib/state-ctrl')
 
 server.use(bodyParser.urlencoded({extended: true}))
 css('tachyons')
 css('./assets/styles/header-bkground.css')
 
-var app = choo()
+const app = choo()
 if (process.env.NODE_ENV !== 'production') {
   app.use(require('choo-devtools')())
   app.use(require('choo-log')())
 }
-// app.use(require('choo-service-worker')())
-
+app.use(require('choo-service-worker')())
 app.route('/', require('./views/main'))
-app.route('/posted', function (data) {
-  console.log('data!')
-  console.log(data)
-  return '<body><h1>hi</h1></body>'
-})
-app.route('/*', require('./views/404'))
+app.route('/posted', require('./views/posted'))
+app.route('/history', require('./views/history'))
 
-if (!module.parent) app.mount('body')
-else module.exports = app
+/* server 
+ */
+server.get('/', (req, res) => {
+    res.send(app.toString('/', {}))
+})
+
+server.post('/posted', dbCtrl.run, stateCtrl.format, (req,  res) => {
+  res.send(app.toString('/posted', { parsed: req.body.parsedState }))
+})
+
+server.get('/history', dbCtrl.get, stateCtrl.hist, (req, res) => {
+  let { success, mappedHistory } = req.body
+  res.send(app.toString('/history', { success:success, history: mappedHistory }))
+})
+
+server.listen(3000)
+
+module.exports = app
